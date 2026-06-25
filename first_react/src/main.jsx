6b793d76b99f82b09c56b2eaf9aea745d9260a0c
AlephContinuum <sparshsamrat@gmail.com>
2026-06-25 16:00:43 +0530

    fixed file structures

--- first_react/src/main.jsx
+++ first_react/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client"
+import "./index.css"
+const root = createRoot(document.getElementById("root"))
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+root.render(
+    <main>
+        <img src="/images.png" width="40px" alt="React logo" />
+        <h1>Fun fact about react!</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+            <li>Was first released in 2013</li>
+        </ul>
+    </main>
 )
